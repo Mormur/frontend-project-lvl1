10fc69c1512c9d8b9b64bebdb6d7caa20585b4f3
marchkatte <marchkatte@gmail.com>
2020-05-05 13:46:50 +0300

    Refactoring: delete an excess condition

--- src/games/check-prime.js
+++ src/games/check-prime.js
@@ -5,9 +5,6 @@ const isPrime = (number) => {
   if (number === 1) {
     return 'no';
   }
-  if (number === 2) {
-    return 'yes';
-  }
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
       return 'no';
